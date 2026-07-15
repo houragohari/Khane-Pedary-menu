61f5d267b9090253f05e9f268660decddd8e3741
houragohari <houragohari@gmail.com>
2026-07-15 03:58:32 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+fetch("menu.json")
+.then(response => response.json())
+.then(data => {
+
+let menu = document.getElementById("menu");
+
+
+data.categories.forEach(category => {
+
+
+let title = document.createElement("h2");
+
+title.innerHTML = category.title;
+
+menu.appendChild(title);
+
+
+
+category.items.forEach(item => {
+
+
+let card = document.createElement("div");
+
+card.className="card";
+
+
+card.innerHTML = `
+
+<h3>${item.name}</h3>
+
+<p>${item.volume || ""}</p>
+
+<p>${item.description}</p>
+
+<div class="price">
+${item.price} هزار تومان
+</div>
+
+`;
+
+
+menu.appendChild(card);
+
+
+});
+
+
+});
+
+
+});
