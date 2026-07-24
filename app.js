c1327f7217e19757ee17cac26ef2d820f05debf8
houragohari <houragohari@gmail.com>
2026-07-24 17:45:24 +0330

    Update app.js

--- app.js
+++ app.js
@@ -2,50 +2,45 @@ fetch("menu.json")
 .then(response => response.json())
 .then(data => {
 
-let menu = document.getElementById("menu");
+    let menu = document.getElementById("menu");
 
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
 
-data.categories.forEach(category => {
+    data.categories.forEach(category => {
 
+        let title = document.createElement("h2");
+        title.innerHTML = category.title;
+        menu.appendChild(title);
 
-let title = document.createElement("h2");
+        category.items.forEach(item => {
 
-title.innerHTML = category.title;
+            let card = document.createElement("div");
+            card.className = "card";
 
-menu.appendChild(title);
+            card.innerHTML = `
+                ${item.image ? `<img src="${item.image}" alt="${item.name}">` : ""}
+                <h3>${item.name}</h3>
+                <p class="volume">${item.volume || ""}</p>
+                <p class="description">${item.description}</p>
+                <div class="price">${item.price} هزار تومان</div>
+            `;
 
+            if(item.image){
+                card.addEventListener("click", () => {
+                    modalImage.src = item.image;
+                    modal.classList.add("show");
+                });
+            }
 
+            menu.appendChild(card);
 
-category.items.forEach(item => {
+        });
 
+    });
 
-let card = document.createElement("div");
-
-card.className="card";
-
-
-card.innerHTML = `
-${item.image ? `<img src="${item.image}" alt="${item.name}" class="menu-image">` : ""}
-
-<h3>${item.name}</h3>
-
-<p>${item.volume || ""}</p>
-
-<p>${item.description}</p>
-
-<div class="price">
-${item.price} هزار تومان
-</div>
-`;
-
-
-menu.appendChild(card);
-
-
-});
-
-
-});
-
+    modal.addEventListener("click", () => {
+        modal.classList.remove("show");
+    });
 
 });
